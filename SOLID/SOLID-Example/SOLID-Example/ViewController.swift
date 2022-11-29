b6461bb6ef68e75c321a8245f909c228e18cd5eb
Bilal <bilalbakhrom@gmail.com>
2022-11-29 21:19:14 +0500

    Add project to implement solid principles

---
+++ SOLID/SOLID-Example/SOLID-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID-Example
+//
+//  Created by Bilal Bakhrom on 29/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
